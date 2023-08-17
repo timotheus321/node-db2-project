7aaf21bbaf7e3c845fee3ae3024014e37f460c86
Timothy White <bigtball321@yahoo.com>
2023-08-17 16:22:30 -0400

    w

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+const cars = [
+    {
+    vin: '123456789101',
+    make: 'toyota',
+    model: 'prius',
+    mileage: 215000,
+    title: 'clean',
+    transmission: 'manual'
+},
+{
+    vin: '123456789101',
+    make: 'toyota',
+    model: 'corolla',
+    mileage: 216000,
+    title: 'salvage',
+    
+},
+{
+    vin: '123456789101',
+    make: 'ford',
+    model: 'focus',
+    mileage: 21500,
+   
+}
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
